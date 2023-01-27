62919a0d210694333b38ad8d736b8067166a3693
David Oster aka George Pasparakis <paspa@hotmail.com>
2023-01-27 19:08:38 +0200

    login is almost ready

---
+++ Controllers/Authentication.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Authentication : ControllerBase
+    {
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Authenticate([FromBody]UserDetails user)
+        {
+            var credentials = new { user.Username, user.Password };
+            return Ok(credentials);
+        }
+    }
+
+    public class UserDetails
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
